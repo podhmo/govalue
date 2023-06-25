cd28b201ca305b55a43e2931ee75042feca97493
podhmo <ababjam61+github@gmail.com>
2023-06-25 14:14:06 +0900

    feat: support int

---
+++ govalue.go
@@ -0,0 +1,66 @@
+package govalue
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+	"strconv"
+	"sync"
+)
+
+var pool = &sync.Pool{
+	New: func() any {
+		return new(bytes.Buffer)
+	},
+}
+
+func ToCode(v any) string {
+	buf := pool.Get().(*bytes.Buffer)
+	buf.Reset()
+	defer pool.Put(buf)
+	if err := writeCode(buf, v); err != nil {
+		return fmt.Sprintf("<%s>", err.Error())
+	}
+	return buf.String()
+}
+
+func writeCode(b *bytes.Buffer, v any) error {
+	rv := reflect.ValueOf(v)
+	rt := reflect.TypeOf(v)
+	switch rt.Kind() {
+	case reflect.Invalid:
+	case reflect.Bool:
+	case reflect.Int:
+		if _, err := b.WriteString(strconv.Itoa(v.(int))); err != nil {
+			return err
+		}
+		return nil
+	case reflect.Int8:
+	case reflect.Int16:
+	case reflect.Int32:
+	case reflect.Int64:
+	case reflect.Uint:
+	case reflect.Uint8:
+	case reflect.Uint16:
+	case reflect.Uint32:
+	case reflect.Uint64:
+	case reflect.Uintptr:
+	case reflect.Float32:
+	case reflect.Float64:
+	case reflect.Complex64:
+	case reflect.Complex128:
+	case reflect.Array:
+	case reflect.Chan:
+	case reflect.Func:
+	case reflect.Interface:
+	case reflect.Map:
+	case reflect.Pointer:
+	case reflect.Slice:
+	case reflect.String:
+	case reflect.Struct:
+	case reflect.UnsafePointer:
+	default:
+		panic(fmt.Sprintf("unexpected kind: %s, %v", rt.Kind(), rv)) // we need panic instead of error
+	}
+	return fmt.Errorf("not implemented")
+}
---
+++ govalue_test.go
@@ -0,0 +1,27 @@
+package govalue_test
+
+import (
+	"testing"
+
+	"github.com/podhmo/govalue"
+)
+
+func TestToCode(t *testing.T) {
+	type args struct {
+		v any
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "int", args:args{v: 10}, want: `10`},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := govalue.ToCode(tt.args.v); got != tt.want {
+				t.Errorf("ToCode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
